c6b2b32096dd8b9d2bf6e30af08a0e9b33e60a4d
John <john@Johns-MacBook-Pro.local>
2018-09-11 22:33:50 -0500

    updated with complete part 2

--- homework/homework_part_2/app.js
+++ homework/homework_part_2/app.js
@@ -1,14 +1,33 @@
 // PART ONE
-// 1. Concatination combines strings (and only strings), whereas interpolation allows the use of placeholder variables that can allow any a range of inputs. 
-// 2. DRY stands for "don't repeat yourself", and it's underpinning idea (creating flexible and reusable code, functions, etc.) is best practice for writing most types of code (we've used it for Javascript and CSS).
-// 3. Declaring a variable is using the var keyword to announce an element as a variable. Defining a variable is giving it value.
-// 4. Redclaring a variable with let allows you to declare that a variable is limited in scope to the block or expression in which it is used. Const variables are a subset of let variables, the values of which cannot be reassigned. Use let for variables that may need to change and const for those that won't?
-// 5. A parent directory is a directory that contains a another directory, which is in turn is known as its child.
-// 6. using the command 'man'.
-// 7. tab completion fills in suggested files/folders based on the first letter/number you put in. It's awesome because it saves you the time of having to type out the whole name. 
 
-// PART TWO
-//const a = 4;
+// 1. Concatenation allows you to add datatypes known as "strings" (text-based) together. 
+// console.log('just ' + 'like ' + 'so!')
+// Interpolation is similar, but allows you to use variables in place of individual strings, which can substitute in a variety of values. 
+// var word = 'like';
+//
+// console.log('just ' + word ' so!') // You do have to space them a little differently, in this case.
+
+// 2. DRY stands for DON'T REPEAT YOURSELF. 
+// Sage wisdom in a handy acronym: basically, 
+// don't write a whole bunch of expressions when you can just write one, i.e. a loop.
+
+// 3. Declaring a variable is using the var keyword to announce an element as a variable. Defining a variable is giving it value.
+// i.e. var bike = blue
+//      ^declaration  ^definition
+
+// 4. Const variables can have their attributes changed, but they CAN'T be redefined. 
+// They should be your default variable type, as their stability is generally useful. 
+// Let variables CAN be redefined. They're helpful when declaring a variable you might like to change like, say, one inside of a loop.  
+
+// 5. A parent directory is a directory that contains a another directory, which is in turn is known as its child.
+
+// 6. using the command 'man'.
+
+// 7. Tab completion fills in suggested files/folders based on the first letter/number you put in. 
+// It's awesome because it saves you the time of having to type out the whole name. 
+
+//  PART TWO
+//  const a = 4;
 //  const b = 53;
 //  const c = 57;
 //  const d = 16;
@@ -16,35 +35,42 @@
 
 // 1. a < b
 // 2. c > d
-// 3. ==
+// 3. ===
 // 4. a < b < c
-// 5. a == a < d
-// 6. e = 'Kevin'
-// 7. 48 = '48' 
+// 5. a === a < d
+// 6. e === 'Kevin'
+// 7. 48 == '48' 
 
 //PART THREE
-// 1. Infinite? I think...
+
+// 1. Infinite?
 // 2. Not infinite. 'Do not run this loop'
-// 3.
+// 3. 
 
 let letters = "A";
 let i = 0;
 
-while (i < 20) { //as long as the index is less than 20 long
-	letters += "A"; //it will add the letter A to the string
+while (i < 20) { //as long as i is less than 20 
+	letters += "A"; //it will add the letter A to the string letters
 	i++;
 }
 
-console.log(letters); //expected output: AAAAAAA <- a long string of that
+console.log(letters); //expected output: a 20-A long string, like "AAAAAAA", etc.
+
+//Worked just like I thought it would :) 
 
 //PART FOUR
-// 1. For loops are useful for looping through each item in an array. While loops continue indefinitely until the condition they are set around becomes false. They can go forever uneless given an method to escape. 
+
+// 1. While loops continue looping until a certain condition is met-say, until variable being fed through the loop is transformed to a certain value.
+// For loops, on the other hand, run through each value in an array and work a repeating process on each one.
+// They both involve a repeating process, and can be used to solve some of the same problems.
+
 // 2.
 for (let i = 0; i <= 999; i ++) {
-	console.log(i + 1);
+	console.log(i);
 }
 // 3. The first part of the control statement is: let i = 0; it specifies that the code should be executed if it is true. 
-// The second part of the control statement is: i < 100; it defines the parameters for which the code should be executed/at which point it will no longe rapply. 
+// The second part of the control statement is: i < 100; it defines the parameters for which the code should be executed/at which point it will no longer apply. 
 // The third part of the control statement is: i ++; this is the code that will add 1 to i repeatedly, in a loop, until the condition set in part 2 is met. 
 // 4. 
 for (let i = 999; i >= 0; i --) {
